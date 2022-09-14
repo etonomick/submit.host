322f68cfff05ba2442936016dfe7850c4d3044a4
Valery Stepanov <etonomick@gmail.com>
2022-09-14 15:16:26 +0000

    cool circles

--- components/SubmissionItem.js
+++ components/SubmissionItem.js
@@ -8,7 +8,7 @@ export default function SubmissionItem({ item }) {
 
     return (
         <Link href={`/forms/${item.form_id}/${item._id}`}><a>
-            <div className={`p-3 ${submission === item._id ? "border-2 border-black shadow-brick-to-r" : "border-2 border-transparent bg-transparent"}`}>
+            <div className={`p-3 ${submission === item._id ? "border-l-2 border-l-black" : "border-2 border-transparent bg-transparent"}`}>
                 <pre className="text-xs line-clamp-5">{JSON.stringify(item.data, null, 2)}</pre>
                 <div className="">{new Date().toLocaleDateString()}</div>
             </div>
--- components/layout.js
+++ components/layout.js
@@ -42,9 +42,9 @@ export default function Layout({ children }) {
     const { t } = useTranslation()
 
     return (
-        <div className="flex flex-col h-screen bg-orange-50/50 font-mono">
+        <div className="flex flex-col h-screen bg-neutral-50 font-mono">
             <div className="flex flex-row place-content-between py-3 items-center 
-             absolute left-0 top-0 right-0 z-50 bg-green-300">
+             absolute left-0 top-0 right-0 z-50">
                 <div className="flex text-3xl font-bold items-center">
                     <Button onClick={() => setVisible(!visible)}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -91,7 +91,7 @@ export default function Layout({ children }) {
                         }}
                         cancel={() => setDeleteDialog(false)} />
 
-                    <div className="overflow-y-scroll flex flex-col h-screen pb-16">
+                    <div className="overflow-y-scroll flex flex-col h-screen pb-16 gap-2 p-2">
                         {forms && forms.map((form, index) => (
                             <FormItem
                                 key={form._id ?? index}
